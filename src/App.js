779c58bac51a33e7a1b2b943537a1768753a4d08
martytrajkoski1 <marti.trajkoski@students.finki.ukim.mk>
2023-04-11 19:43:17 +0200

    aboutUs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';  
 import NavigationMenu from './components/navigationMenu/navigationMenu'
 import LandingPage from './components/login/landingPage'
 import Signin from './components/login/signin'
 import Register from './components/login/register'
+import AboutUs from './components/aboutUs/aboutus';
 
-import 'bootstrap/dist/css/bootstrap.min.css';  
+
 export function App()
 {
 return(
   <div>
     {/* <NavigationMenu></NavigationMenu>
     <h1>икт проект</h1> */}
-    <LandingPage></LandingPage>
+    {/* <LandingPage></LandingPage> */}
     {/* <Signin></Signin> */}
     {/* <Register></Register> */}
+    <AboutUs></AboutUs>
   </div>
 )
 }
---
+++ src/components/aboutUs/aboutus.js
@@ -0,0 +1,35 @@
+import { Button, Container, Row, Col } from "react-bootstrap";
+import NavigationMenu from '../navigationMenu/navigationMenu'
+import "../../style/AboutUs.scss"
+
+function AboutUs(){
+    return(
+        <div className="aboutUs">
+            <NavigationMenu/>
+            <Container fluid>
+                <Row>
+                    <Col xl={6} lg={6} md={6} className="media">
+                        <div>
+                            <img src={require("../../media/aboutUs/—Pngtree—e-learning illustration of lecturer_6579796.png")}/>
+                        </div>
+                        
+                    </Col>
+                    <Col xl={6} lg={6} md={6} className="content">
+                        <div className="text">
+                            <p>
+                                At LernDeutsch, we’ve seen again and again how the seemingly simple act of creating can be a force for growth, change, 
+                                and discovery in people’s lives. We want to inspire and multiply the kind of creative exploration that furthers expression,
+                                learning, and application.
+                            </p>
+                            <p>
+                                LernDeutsch is an online learning community with classes for creative and curious people. On LernDeutsch, members come together 
+                                to find inspiration and take the next step in their creative journey.
+                            </p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
+export default AboutUs;
