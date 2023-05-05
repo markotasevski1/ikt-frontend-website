0913711f1f7bb0b6fad84a99f9f15d1940dff9a7
martytrajkoski1 <marti.trajkoski@students.finki.ukim.mk>
2023-05-05 21:21:03 +0200

    fix Levels and Materials UI

--- src/components/material/Material.js
+++ src/components/material/Material.js
@@ -15,5 +15,6 @@ export default function Material({courseCounter}){
                 </Card.Body>
             </Card>   
         </div>
+        // jukfsejkdsfjkfdjkdsfkjdfsknjdsfknsdfkjdfsjkdsf
     );
 }
