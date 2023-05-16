d370c13ff4f688991ed4231579362a8afbac8c18
martytrajkoski1 <marti.trajkoski@students.finki.ukim.mk>
2023-05-16 02:01:07 +0200

    certificate pdf download

--- src/components/certificate/Certificate.js
+++ src/components/certificate/Certificate.js
@@ -1,14 +1,12 @@
 import NavigationMenu from '../navigationMenu/navigationMenu'
 import { CertificateCard } from './CertificateCard'
 import { CertificateHeader } from './CertificateHeader'
-import CertificateFooter from './CertificateFooter'
 export default function Certificate () {
   return (
     <div>
       <NavigationMenu />
       <CertificateHeader />
       <CertificateCard />
-      <CertificateFooter />
     </div>
   )
 }
--- src/components/certificate/CertificateCard.js
+++ src/components/certificate/CertificateCard.js
@@ -1,21 +1,38 @@
-import {Row, Col, Container } from "react-bootstrap";
+import {Row, Col, Container, Button } from "react-bootstrap";
 import "../../style/Certificate.scss"
+import { useReactToPrint } from "react-to-print";
+import { useRef } from "react";
+
 
 export function CertificateCard(){
 
+    const certificate = useRef();
+    const generate = useReactToPrint({
+        content: () => certificate.current,
+        documentTitle: 'LernDeutsch Certificate',
+        onAfterPrint: () => alert('Saved successfully'),
+        
+    });
+
     return(
-        <Container className="certificateCard">
-            <Row>
-                <Col md={3} lg={3} className="sideSection">
-                    <img src={require("../../media/navigationMenu/LernDeutsch (2).png")}/>
-                </Col>
-                <Col className="cardContent">
-                    <h2>CERTIFICATE OF COMPLETION</h2>
-                    <p>This certificate of completion is awardet to</p>
-                    <p style={{fontWeight: "bold"}}>TEAM 8</p>
-                    <p>in recognition of having sucessfully completed the online course lession</p>
-                </Col>
-            </Row>
-        </Container>
+        <div>
+            <Container className="certificateCard" ref={certificate}>
+                <Row>
+                    <Col md={3} lg={3} className="sideSection">
+                        <img src={require("../../media/navigationMenu/LernDeutsch (2).png")}/>
+                    </Col>
+                    <Col className="cardContent">
+                        <h2>CERTIFICATE OF COMPLETION</h2>
+                        <p>This certificate of completion is awardet to</p>
+                        <p style={{fontWeight: "bold"}}>TEAM 8</p>
+                        <p>in recognition of having sucessfully completed the online course lession</p>
+                    </Col>
+                </Row>
+            </Container>
+            <div className="footer">
+                <Button onClick={generate}>Download</Button>
+            </div>
+        </div>
+        
     );
 }
