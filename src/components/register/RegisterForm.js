92462567c7eebefb6f384f773f7d351b3d54dba8
Marko Tasevski <60316027+markotasevski1@users.noreply.github.com>
2023-05-19 19:02:54 +0200

    Feature login implementation (#23)

--- src/components/register/RegisterForm.js
+++ src/components/register/RegisterForm.js
@@ -36,8 +36,8 @@ export default function RegisterForm() {
       console.log(response.status)
       console.log(response.accessToken)
       if (response.status === 200) {
-        // Redirect the user to the desired page
-        navigate('/home');
+        // Redirect the user to the desired page.
+        navigate('/login');
       }
     } catch (error) {
       if (!error.response) {
