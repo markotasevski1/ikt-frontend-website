dcba78ebb669c1dd7bfbd8149aa99f8830467a23
markotasevski1 <markotasevski123@yahoo.com>
2023-05-24 22:47:36 +0200

    Added axios call in materialSection

--- src/components/material/MaterialSection.js
+++ src/components/material/MaterialSection.js
@@ -1,6 +1,32 @@
 import Material from "./Material";
+import axios from '../../api/axios'
+import { useState, useEffect } from 'react'
+const COURSES_URL = '/courses'
 
 export function MaterialSection(){
+
+    const [courses, setCourses] = useState([])
+    const authTokenSession = sessionStorage.getItem('token')
+    console.log(authTokenSession)
+    
+    useEffect(() => {
+      const fetchCourses = async () => {
+        try {
+          const headers = {
+            Authorization: `Bearer ${authTokenSession}`,
+          }
+    
+          const response = await axios.get(COURSES_URL, { headers })
+          setCourses(response.data)
+          console.log(response.data)
+        } catch (error) {
+          console.error(error)
+        }
+      }
+    
+      fetchCourses()
+    }, []) // Empty dependency array ensures the effect runs only once
+
     const materialComponents = [];
 
     for (let i=1; i<5; i++){
