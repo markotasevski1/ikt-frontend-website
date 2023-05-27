b9a3656ef12f53bdc9029a22448dffdeca33c259
Filip Jovanov <filip.jovanov@students.finki.ukim.mk>
2023-05-27 22:23:41 +0200

    Add redirects for levels

--- src/pages/Materials.js
+++ src/pages/Materials.js
@@ -1,12 +1,20 @@
-import { MaterialSection } from "../components/material/MaterialSection"
+import {MaterialSection} from "../components/material/MaterialSection"
 import NavigationMenu from "../components/navigationMenu/navigationMenu"
+import {useLocation} from "react-router-dom";
 
-export default function Materials(){
-    return(
+export default function Materials() {
+    const location = useLocation();
+    const {state} = location;
+    let levelId = null;
+    if(state != null) {
+        levelId = state.levelId;
+    }
+
+    return (
         <div>
             <NavigationMenu/>
             <div className="materialsPageStyling">
-                <MaterialSection/>
+                <MaterialSection levelId={levelId}/>
             </div>
         </div>
     )
